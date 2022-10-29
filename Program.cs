1a2c911281ceb23ba8241f1f61371ed8ce8c3fc0
rbpolim <rodrigobpolim@gmail.com>
2022-10-29 01:21:39 -0300

    feat: push project into gh

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class Program {
+  public static void Main(string[] args) {
+    string gasoline;
+    string alcohol;
+    string average;
+
+    Console.WriteLine("Digite o preço da gasolina: ");
+    gasoline = Console.ReadLine();
+
+    Console.WriteLine("Digite o preço do álcool: ");
+    alcohol = Console.ReadLine();
+
+    average = (Convert.ToDouble(gasoline) * 0.7) > Convert.ToDouble(alcohol) 
+    ? "Álcool" 
+    : "Gasolina";
+
+    Console.WriteLine("A opção mais vantajosa é: " + average);
+
+    Console.WriteLine("Pressione qualquer tecla para sair...");
+
+    Console.ReadKey();
+  }
+}
